1217454fa416a6cd9894617af48c1d2bc51a77c5
takatori <tktr3w5e0@gmail.com>
2016-05-08 23:11:13 +0900

    Avoid js semi warning

--- inits/21-js2-mode.el
+++ inits/21-js2-mode.el
@@ -2,3 +2,7 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp$" . js2-mode))
+
+;; セミコロンがなくても警告を出さないようにする
+(setq-default js2-strict-missing-semi-warning nil)
+
