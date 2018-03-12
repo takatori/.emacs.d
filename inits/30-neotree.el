63d820e0a82855089fe764667057c8f546fc1e00
takatori <takatorix@gmail.com>
2018-03-12 23:49:22 +0900

    add neotree config

--- inits/30-neotree.el
+++ inits/30-neotree.el
@@ -1,2 +1,24 @@
 (require 'neotree)
+
+;; all-the-icons
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+;; 隠しファイルをデフォルトで表示
+(setq neo-show-hidden-files t)
+
+;; neotree でファイルを新規作成した後、自動的にファイルを開く
+(setq neo-create-file-auto-open t)
+
+;; delete-other-window で neotree ウィンドウを消さない
+(setq neo-persist-show t)
+
+;; neotree ウィンドウを表示する毎に current file のあるディレクトリを表示する
+(setq neo-smart-open t)
+
+;; デフォルトのウィンドウサイズ
+(setq neo-window-width 30)
+
+;; オートリフレッシュ
+(setq neo-autorefresh t)
+
+
