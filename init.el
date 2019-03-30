944bf9f31d0b29f74292b2c5092eb0ed253461f4
鷹取敏志 <takatorix@gmail.com>
2019-03-31 01:47:19 +0900

    add lsp

--- init.el
+++ init.el
@@ -240,6 +240,7 @@
 (use-package auto-save-buffers-enhanced
   :config
   (setq auto-save-buffers-enhanced-interval 1)
+  (setq auto-save-buffers-enhanced-quiet-save-p t)
   (auto-save-buffers-enhanced t))
 
 (use-package avy
@@ -253,6 +254,9 @@
   :config
   (beacon-mode 1))
 
+(use-package col-highlight
+  :bind* ("C-h h" . flash-column-highlight))
+
 (use-package counsel
   :after ivy
   :demand t
@@ -261,6 +265,7 @@
            (concat "\\(\\`\\.[^.]\\|"
                    (regexp-opt completion-ignored-extensions)
                    "\\'\\)"))
+  (counsel-yank-pop-separator "\n-------\n")
   :bind (("C-*"     . counsel-org-agenda-headlines)
          ("C-x C-f" . counsel-find-file)
          ("C-c e l" . counsel-find-library)
@@ -270,7 +275,7 @@
          ("C-h f"   . counsel-describe-function)
          ("C-x r b" . counsel-bookmark)
          ("M-x"     . counsel-M-x)
-         ;; ("M-y"     . counsel-yank-pop)
+         ("M-y"     . counsel-yank-pop)
 
          ("M-s f" . counsel-file-jump)
          ;; ("M-s g" . counsel-rg)
@@ -459,6 +464,85 @@
         ivy-rich-switch-buffer-align-virtual-buffer t
         ivy-rich-path-style 'abbrev))
 
+(use-package lsp-mode
+  :custom
+  ;; debug
+  (lsp-print-io nil)
+  (lsp-trace nil)
+  (lsp-print-performance nil)
+  ;; general
+  (lsp-auto-guess-root t)
+  (lsp-document-sync-method 'incremental) ;; always send incremental document
+  (lsp-response-timeout 5)
+  (lsp-prefer-flymake 'flymake)
+  (lsp-enable-completion-at-point nil)
+  :hook
+  (go-mode . lsp)
+  :bind
+  (:map lsp-mode-map
+  ("C-c r"   . lsp-rename))
+  :config
+  (require 'lsp-clients)
+  ;; LSP UI tools
+  (use-package lsp-ui
+    :custom
+    ;; lsp-ui-doc
+    (lsp-ui-doc-enable t)
+    (lsp-ui-doc-header t)
+    (lsp-ui-doc-include-signature t)
+    (lsp-ui-doc-position 'top) ;; top, bottom, or at-point
+    (lsp-ui-doc-max-width 150)
+    (lsp-ui-doc-max-height 30)
+    (lsp-ui-doc-use-childframe t)
+    (lsp-ui-doc-use-webkit t)
+    ;; lsp-ui-flycheck
+    (lsp-ui-flycheck-enable nil)
+    ;; lsp-ui-sideline
+    (lsp-ui-sideline-enable nil)
+    (lsp-ui-sideline-ignore-duplicate t)
+    (lsp-ui-sideline-show-symbol t)
+    (lsp-ui-sideline-show-hover t)
+    (lsp-ui-sideline-show-diagnostics nil)
+    (lsp-ui-sideline-show-code-actions nil)
+    ;; lsp-ui-imenu
+    (lsp-ui-imenu-enable nil)
+    (lsp-ui-imenu-kind-position 'top)
+    ;; lsp-ui-peek
+    (lsp-ui-peek-enable t)
+    (lsp-ui-peek-peek-height 20)
+    (lsp-ui-peek-list-width 50)
+    (lsp-ui-peek-fontify 'on-demand) ;; never, on-demand, or always
+    :preface
+    (defun ladicle/toggle-lsp-ui-doc ()
+      (interactive)
+      (if lsp-ui-doc-mode
+        (progn
+          (lsp-ui-doc-mode -1)
+          (lsp-ui-doc--hide-frame))
+         (lsp-ui-doc-mode 1)))
+    :bind
+    (:map lsp-mode-map
+    ("C-c C-r" . lsp-ui-peek-find-references)
+    ("C-c C-j" . lsp-ui-peek-find-definitions)
+    ("C-c i"   . lsp-ui-peek-find-implementation)
+    ("C-c m"   . lsp-ui-imenu)
+    ("C-c s"   . lsp-ui-sideline-mode)
+    ("C-c d"   . ladicle/toggle-lsp-ui-doc))
+    :hook
+    (lsp-mode . lsp-ui-mode))
+  ;; Lsp completion
+  (use-package company-lsp
+    :custom
+    (company-lsp-cache-candidates t) ;; always using cache
+    (company-lsp-async t)
+    (company-lsp-enable-recompletion nil)))
+
+;; cclsは別途hookする
+(use-package ccls
+  :custom (ccls-executable "/usr/local/bin/ccls")
+  :hook ((c-mode c++-mode objc-mode) .
+         (lambda () (require 'ccls) (lsp))))
+
 (use-package magit
   :bind (("C-x g" . magit-status)
          ("C-x G" . magit-status-with-prefix))
@@ -580,6 +664,22 @@
 (use-package rainbow-delimiters
   :hook
   (prog-mode . rainbow-delimiters-mode))
+
+(use-package swiper
+  :after ivy
+  :bind* ("C-s" . swiper)
+  :bind (:map swiper-map
+              ("M-y" . yank)
+              ("M-%" . swiper-query-replace)
+              ("C-." . swiper-avy)
+              ("M-c" . swiper-mc)))
+
+
+(use-package vlf
+  :disabled t
+  :defer 5
+  :init
+  (setq-default vlf-tune-max (* 512 1024)))
 
 (use-package volatile-highlights
   :diminish
