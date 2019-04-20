7417e936f929d52fd8fd92cd586bed4e6876da78
鷹取敏志 <takatorix@gmail.com>
2019-04-20 15:46:22 +0900

    update company-box

--- init.el
+++ init.el
@@ -585,6 +585,7 @@
   :custom
   (company-idle-delay 0)
   (company-echo-delay 0)
+  (company-minimum-prefix-length 1)      
   :hook
   (after-init . global-company-mode)
   ((go-mode typescript-mode c++-mode) . (lambda () (set (make-local-variable 'company-backends)
@@ -595,56 +596,62 @@
 			       ;; company-ispell
 			       )))))
   :config
+  (use-package company-posframe
+    :hook (company-mode . company-posframe-mode))  
+  ;; Show pretty icons
   ;; Show pretty icons
   (use-package company-box
     :diminish
-    :functions (all-the-icons-faicon
-		all-the-icons-material
-		all-the-icons-octicon
-		all-the-icons-alltheicon)
     :hook (company-mode . company-box-mode)
-    :custom
-    (company-box-backends-colors nil)
-    (company-box-enable-icon (display-graphic-p))
+    :init (setq company-box-icons-alist 'company-box-icons-all-the-icons)
     :config
+    (setq company-box-backends-colors nil)
+    (setq company-box-show-single-candidate t)
+    (setq company-box-max-candidates 50)
+
+    (defun company-box-icons--elisp (candidate)
+      (when (derived-mode-p 'emacs-lisp-mode)
+        (let ((sym (intern candidate)))
+          (cond ((fboundp sym) 'Function)
+                ((featurep sym) 'Module)
+                ((facep sym) 'Color)
+                ((boundp sym) 'Variable)
+                ((symbolp sym) 'Text)
+                (t . nil)))))
+
     (with-eval-after-load 'all-the-icons
-      (setq company-box-icons-unknown
-	    (all-the-icons-material "text_fields" :v-adjust -0.2))
-      (setq company-box-icons-elisp
-	    (list
-	     (all-the-icons-faicon "cube" :v-adjust -0.0575 :face 'font-lock-constant-face)                     ; Function
-	     (all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-warning-face)                       ; Variable
-	     (all-the-icons-faicon "cubes" :height 0.8 :v-adjust -0.0575 :face 'font-lock-warning-face)         ; Feature
-	     (all-the-icons-material "palette" :v-adjust -0.2)))
-      (setq company-box-icons-yasnippet
-	    (all-the-icons-octicon "code" :v-adjust -0.0575 :face 'font-lock-variable-name-face))               ; Yasnippet
-      (setq company-box-icons-lsp
-	    `(( 1  . ,(all-the-icons-material "text_fields" :v-adjust -0.2))                                    ; Text
-	      ( 2  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575 :face font-lock-constant-face))            ; Method
-	      ( 3  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575 :face font-lock-constant-face))            ; Function
-	      ( 4  . ,(all-the-icons-faicon "cube" :v-adjust -0.0575 :face font-lock-constant-face))            ; Constructor
-	      ( 5  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-warning-face))             ; Field
-	      ( 6  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-warning-face))             ; Variable
-	      ( 7  . ,(all-the-icons-faicon "cubes" :height 0.9 :v-adjust -0.0575 :face 'font-lock-constant-face))             ; Class
-	      ( 8  . ,(all-the-icons-faicon "cubes" :height 0.9 :v-adjust -0.0575))            ; Interface
-	      ( 9  . ,(all-the-icons-octicon "package" :v-adjust -0.0575 :face 'font-lock-warning-face))                             ; Module
-	      (10  . ,(all-the-icons-faicon "wrench" :v-adjust -0.0575))                                        ; Property
-	      (11  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))                                           ; Unit
-	      (12  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-constant-face))             ; Value
-	      (13  . ,(all-the-icons-faicon "tags" :v-adjust -0.0575 :face 'font-lock-constant-face))           ; Enum
-	      (14  . ,(all-the-icons-material "format_align_center" :v-adjust -0.2))                            ; Keyword
-	      (15  . ,(all-the-icons-octicon "code" :v-adjust -0.0575 :face 'font-lock-variable-name-face))     ; Snippet
-	      (16  . ,(all-the-icons-material "palette" :v-adjust -0.2))                                        ; Color
-	      (17  . ,(all-the-icons-faicon "file-o" :v-adjust -0.0575))                                        ; File
-	      (18  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))                                           ; Reference
-	      (19  . ,(all-the-icons-octicon "file-directory" :v-adjust -0.0575))                               ; Folder
-	      (20  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575 :face 'font-lock-keyword-face))             ; EnumMember
-	      (21  . ,(all-the-icons-faicon "tag" :v-adjust -0.0575))                                           ; Constant
-	      (22  . ,(all-the-icons-faicon "cubes" :height 0.9 :v-adjust -0.0575 :face 'font-lock-constant-face)) ; Struct
-	      (23  . ,(all-the-icons-faicon "bolt" :v-adjust -0.0575 :face 'font-lock-warning-face))               ; Event
-	      (24  . ,(all-the-icons-octicon "tag" :v-adjust -0.0575))                                             ; Operator
-	      (25  . ,(all-the-icons-octicon "tag" :v-adjust -0.0575 :face 'font-lock-const-face))                 ; TypeParameter
-	      ))))
+      (declare-function all-the-icons-faicon 'all-the-icons)
+      (declare-function all-the-icons-fileicon 'all-the-icons)
+      (declare-function all-the-icons-material 'all-the-icons)
+      (declare-function all-the-icons-octicon 'all-the-icons)
+      (setq company-box-icons-all-the-icons
+            `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.7 :v-adjust -0.15))
+              (Text . ,(all-the-icons-faicon "book" :height 0.68 :v-adjust -0.15))
+              (Method . ,(all-the-icons-faicon "cube" :height 0.7 :v-adjust -0.05 :face 'font-lock-constant-face))
+              (Function . ,(all-the-icons-faicon "cube" :height 0.7 :v-adjust -0.05 :face 'font-lock-constant-face))
+              (Constructor . ,(all-the-icons-faicon "cube" :height 0.7 :v-adjust -0.05 :face 'font-lock-constant-face))
+              (Field . ,(all-the-icons-faicon "tags" :height 0.65 :v-adjust -0.15 :face 'font-lock-warning-face))
+              (Variable . ,(all-the-icons-faicon "tag" :height 0.7 :v-adjust -0.05 :face 'font-lock-warning-face))
+              (Class . ,(all-the-icons-faicon "clone" :height 0.65 :v-adjust 0.01 :face 'font-lock-constant-face))
+              (Interface . ,(all-the-icons-faicon "clone" :height 0.65 :v-adjust 0.01))
+              (Module . ,(all-the-icons-octicon "package" :height 0.7 :v-adjust -0.15))
+              (Property . ,(all-the-icons-octicon "package" :height 0.7 :v-adjust -0.05 :face 'font-lock-warning-face)) ;; Golang module
+              (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.7 :v-adjust -0.15))
+              (Value . ,(all-the-icons-material "format_align_right" :height 0.7 :v-adjust -0.15 :face 'font-lock-constant-face))
+              (Enum . ,(all-the-icons-material "storage" :height 0.7 :v-adjust -0.15 :face 'all-the-icons-orange))
+              (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.7 :v-adjust -0.15))
+              (Snippet . ,(all-the-icons-faicon "code" :height 0.7 :v-adjust 0.02 :face 'font-lock-variable-name-face))
+              (Color . ,(all-the-icons-material "palette" :height 0.7 :v-adjust -0.15))
+              (File . ,(all-the-icons-faicon "file-o" :height 0.7 :v-adjust -0.05))
+              (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.7 :v-adjust -0.15))
+              (Folder . ,(all-the-icons-octicon "file-directory" :height 0.7 :v-adjust -0.05))
+              (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.7 :v-adjust -0.15 :face 'all-the-icons-blueb))
+              (Constant . ,(all-the-icons-faicon "tag" :height 0.7 :v-adjust -0.05))
+              (Struct . ,(all-the-icons-faicon "clone" :height 0.65 :v-adjust 0.01 :face 'font-lock-constant-face))
+              (Event . ,(all-the-icons-faicon "bolt" :height 0.7 :v-adjust -0.05 :face 'all-the-icons-orange))
+              (Operator . ,(all-the-icons-fileicon "typedoc" :height 0.65 :v-adjust 0.05))
+              (TypeParameter . ,(all-the-icons-faicon "hashtag" :height 0.65 :v-adjust 0.07 :face 'font-lock-const-face))
+              (Template . ,(all-the-icons-faicon "code" :height 0.7 :v-adjust 0.02 :face 'font-lock-variable-name-face))))))
     ;; Show quick tooltip
     (use-package company-quickhelp
       :defines company-quickhelp-delay
